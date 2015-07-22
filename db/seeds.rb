872b564c636447bf3b9898bd8a55944440b50d4e
Mark Chavez <mccd732@gmail.com>
2015-07-22 12:02:01 -0700

    started seeding, categorizations mayhem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-TagCategory.create([{ name: 'Drive-train'}, { name: "Cosmetic"}, { name: "Configuration" }])
+TagCategory.create([{ id: 1, name: 'Rear Cassette/Freewheel/Internal'}, { id: 2, name: "Cosmetic"}, { id: 3, name: "Configuration" }])
+
+Tag.create([{ name: '1sp', tag_category_id: 1 }, { name: '2sp', tag_category_id: 1 },{ name: '3sp', tag_category_id: 1 },
+  { name: '4sp', tag_category_id: 1 },{ name: '5sp', tag_category_id: 1 }, { name: '6sp', tag_category_id: 1 },
+  { name: '7sp', tag_category_id: 1 }, { name: '8sp', tag_category_id: 1 }, { name: '9sp', tag_category_id: 1 },
+  { name: '9sp', tag_category_id: 1 }, { name: '10sp', tag_category_id: 1 }, { name: '11sp', tag_category_id: 1 },
+  { name: 'Shimano', tag_category_id: 3 }, { name: 'Campagnolo', tag_category_id: 3 }, { name: 'SRAM', tag_category_id: 3 },
+  { name: 'Suntour', tag_category_id: 3 },
+  ])
